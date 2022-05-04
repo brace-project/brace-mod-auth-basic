a786d539c06f6214c4dc6d98a20b2c1ff8011964
Matthias <m@tth.es>
2022-05-05 00:31:18 +0200

    validate token Middleware

---
+++ src/RequireValidAuthTokenMiddleware.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Brace\Auth\Basic;
+
+use Brace\Core\Base\BraceAbstractMiddleware;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class RequireValidAuthTokenMiddleware extends BraceAbstractMiddleware
+{
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $authToken = $this->app->get("basicAuthToken", BasicAuthToken::class);
+        $authToken->validate();
+        return $handler->handle($request);
+    }
+}
